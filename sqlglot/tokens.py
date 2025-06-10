98116e63fd1c74843c24b29df57557430ee6f1ea
inancdokurel <inanc.dokurel@globalmaksimum.com>
2025-06-10 19:29:13 +0300

    Add support for COLON_GT token in parser and tokenizer
    - Updated the parser to handle the COLON_GT token in addition to DCOLON.
    - Introduced COLON_GT token in the TokenType class.
    - Enhanced the tokenizer to recognize the ":>" string as COLON_GT.

--- sqlglot/tokens.py
+++ sqlglot/tokens.py
@@ -38,6 +38,7 @@ class TokenType(AutoName):
     PLUS = auto()
     COLON = auto()
     DCOLON = auto()
+    COLON_GT = auto()
     DQMARK = auto()
     SEMICOLON = auto()
     STAR = auto()
@@ -662,6 +663,7 @@ class Tokenizer(metaclass=_Tokenizer):
         HINT_START: TokenType.HINT,
         "==": TokenType.EQ,
         "::": TokenType.DCOLON,
+        ":>": TokenType.COLON_GT,
         "||": TokenType.DPIPE,
         ">=": TokenType.GTE,
         "<=": TokenType.LTE,
